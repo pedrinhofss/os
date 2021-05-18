d7c337df259dc2dbc951b42055d7125d74b777bd
Pedro Francisco <pedrinhofss2010@gmail.com>
2021-05-18 17:00:41 -0300

    a

--- boot_loader.asm
+++ boot_loader.asm
@@ -1,17 +1,16 @@
-mov ah, 0x0e
-mov al, 65
-int 0x10
+read_char:
+    mov ah, 0x0h
+    int 0x16h
+    ret
+
+write_char
 
 loop:
-    mov al, 0x10
+    mov ah, 0
     int 0x16
+    mov ah, 0x0e
     int 0x10
     jmp loop
-
-
-
-
-
 
 jmp $
 times 510-($-$$) db 0
